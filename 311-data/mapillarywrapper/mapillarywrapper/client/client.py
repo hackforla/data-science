4de7ef2fef9c9b9fc29c20a733699db0666d26d6
AlbertUlysses <albertulysseschavez@gmail.com>
2020-08-26 16:43:36 -0700

    bug: pagination

--- 311-data/mapillarywrapper/mapillarywrapper/client/client.py
+++ 311-data/mapillarywrapper/mapillarywrapper/client/client.py
@@ -27,9 +27,34 @@ class MapClient():
                     f'client_id={self.CLIENT_ID}&sort_by=key'
         complete_url = self.BASE_DOMAIN + params
         response = requests.get(complete_url)
-        response.links['next']
         if response.status_code != 200:
             return error_response(response)
-        json_dict = response.json()
-        transform_json = usecase.Properties()
-        return transform_json.transform_json_(json_dict)
+        url_links = [response.links['first']['url']]
+        current_link = response.links
+        while 'next' in current_link:
+            next_url = current_link['next']['url']
+            url_links.append(next_url)
+            new_response = requests.get(next_url)
+            if new_response.status_code != 200:
+                return error_response(response)
+            current_link = new_response.links
+        transform_list = []
+        temp_transform = usecase.Properties()
+        for i, v in enumerate(url_links):
+            temp_response = requests.get(v)
+            temp_json = temp_response.json()
+            transform_list += temp_transform.transform_json_(temp_json)
+        return transform_list
+        """Ignore the following lines, I will go back and delete them"""
+        #response_test = response1.links
+        #print(response_test)
+        #json_dict = response1.json()
+        #print(len(json_dict))
+        #response2 = requests.get(response.links['next']['url'])
+        #print(response2.links)
+        #json_2 = response2.json()
+        #print(len(json_2))
+        #transform_json = usecase.Properties()
+        #transform_list = transform_json.transform_json_(json_dict) + transform_json.transform_json_(json_2)
+        # transform_json.transform_json_(json_dict)
+        #return transform_list 
