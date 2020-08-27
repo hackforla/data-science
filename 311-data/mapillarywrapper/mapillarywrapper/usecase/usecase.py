10b8a2ec162fb7863790c4cf5c83b2f250584a1f
AlbertUlysses <albertulysseschavez@gmail.com>
2020-08-26 22:51:38 -0700

    feat: los angeles stop sign dataset

--- 311-data/mapillarywrapper/mapillarywrapper/usecase/usecase.py
+++ 311-data/mapillarywrapper/mapillarywrapper/usecase/usecase.py
@@ -10,21 +10,24 @@ class Properties():
         sub_json_ = json_dict["features"]
         new_json_list = []
         for collection in range(len(sub_json_)):
-            accuracy = sub_json_[collection]['properties']['accuracy']
-            altitude = sub_json_[collection]['properties']['altitude']
-            direction = sub_json_[collection]['properties']['direction']
-            first_seen_at = sub_json_[collection]['properties']['first_seen_at']
-            last_seen_at = sub_json_[collection]['properties']['last_seen_at']
-            key = sub_json_[collection]['properties']['key']
-            layer = sub_json_[collection]['properties']['layer']
-            value = sub_json_[collection]['properties']['value']
-            coordinates = sub_json_[collection]['geometry']['coordinates']
-            geometry_type = sub_json_[collection]['geometry']['type']
-            traffic_sign = ts.TrafficSign(accuracy, altitude, direction, 
-                                          first_seen_at, last_seen_at, key, 
-                                          layer, value, coordinates, 
-                                          geometry_type)
-            new_json_list.append(traffic_sign.to_dict())
+            try:
+                accuracy = sub_json_[collection]['properties']['accuracy']
+                altitude = sub_json_[collection]['properties']['altitude']
+                direction = sub_json_[collection]['properties']['direction']
+                first_seen_at = sub_json_[collection]['properties']['first_seen_at']
+                last_seen_at = sub_json_[collection]['properties']['last_seen_at']
+                key = sub_json_[collection]['properties']['key']
+                layer = sub_json_[collection]['properties']['layer']
+                value = sub_json_[collection]['properties']['value']
+                coordinates = sub_json_[collection]['geometry']['coordinates']
+                geometry_type = sub_json_[collection]['geometry']['type']
+                traffic_sign = ts.TrafficSign(accuracy, altitude, direction, 
+                                            first_seen_at, last_seen_at, key, 
+                                            layer, value, coordinates, 
+                                            geometry_type)
+                new_json_list.append(traffic_sign.to_dict())
+            except Exception:
+                pass
         return new_json_list
 
             
