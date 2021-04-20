04e31503ad441a77bc81da5979cd9855b8cc56b2
AlbertUlysses <albertulysseschavez@gmail.com>
2021-04-20 12:52:05 -0700

    chore: clean up modules

--- 311-data/mapillarywrapper/mapillarywrapper/domain/trafficsign.py
+++ 311-data/mapillarywrapper/mapillarywrapper/domain/trafficsign.py
@@ -1,8 +1,17 @@
-class TrafficSign():
-
-    def __init__(self, accuracy: float, altitude: float, direction: float,
-                 first_seen_at: str, last_seen_at: str, key: str, layer: str,
-                 value: str, coordinates: list, geometry_type: str) -> None:
+class TrafficSign:
+    def __init__(
+        self,
+        accuracy: float,
+        altitude: float,
+        direction: float,
+        first_seen_at: str,
+        last_seen_at: str,
+        key: str,
+        layer: str,
+        value: str,
+        coordinates: list,
+        geometry_type: str,
+    ) -> None:
         self.accuracy = accuracy
         self.altitude = altitude
         self.direction = direction
@@ -18,29 +27,29 @@ class TrafficSign():
     def from_dict(cls, adict: dict) -> None:
         """ returns an model object from a dictionary."""
         return cls(
-                accuracy=adict['accuracy'],
-                altitude=adict['altitude'],
-                direction=adict['direction'],
-                first_seen_at=adict['first_seen_at'],
-                last_seen_at=adict['last_seen_at'],
-                key=adict['key'],
-                layer=adict['layer'],
-                value=adict['value'],
-                coordinates=adict['coordinates'],
-                geometry_type=adict['geometry_type']
-                )
+            accuracy=adict["accuracy"],
+            altitude=adict["altitude"],
+            direction=adict["direction"],
+            first_seen_at=adict["first_seen_at"],
+            last_seen_at=adict["last_seen_at"],
+            key=adict["key"],
+            layer=adict["layer"],
+            value=adict["value"],
+            coordinates=adict["coordinates"],
+            geometry_type=adict["geometry_type"],
+        )
 
     def to_dict(self) -> dict:
         """ returns a dictionary of the model TrafficSign"""
         return {
-                'accuracy': self.accuracy,
-                'altitude': self.altitude,
-                'direction': self.direction,
-                'first_seen_at': self.first_seen_at,
-                'last_seen_at': self.last_seen_at,
-                'key': self.key,
-                'layer': self.layer,
-                'value': self.value,
-                'coordinates': self.coordinates,
-                'geometry_type': self.geometry_type
-                }
+            "accuracy": self.accuracy,
+            "altitude": self.altitude,
+            "direction": self.direction,
+            "first_seen_at": self.first_seen_at,
+            "last_seen_at": self.last_seen_at,
+            "key": self.key,
+            "layer": self.layer,
+            "value": self.value,
+            "coordinates": self.coordinates,
+            "geometry_type": self.geometry_type,
+        }
