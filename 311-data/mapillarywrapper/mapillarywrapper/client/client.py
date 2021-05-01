92d501aada2788e976b7f96376f9e97eca788f4b
Henry Kaplan <50933869+henrykaplan@users.noreply.github.com>
2021-05-01 16:19:37 -0700

    Add "layer" param to request more Mapillary layers

--- 311-data/mapillarywrapper/mapillarywrapper/client/client.py
+++ 311-data/mapillarywrapper/mapillarywrapper/client/client.py
@@ -8,25 +8,29 @@ def error_response(response):
     """
     print(response)
 
-
 class MapClient:
     def __init__(self, YOUR_CLIENT_ID: str) -> None:
         self.CLIENT_ID = YOUR_CLIENT_ID
         self.BASE_DOMAIN = "https://a.mapillary.com/v3/map_features"
 
     def trafficinfo(
-        self, lowerbbox: list, upperbbox: list, perpage: int = 1, value=None
+        self,
+        lowerbbox: list,
+        upperbbox: list,
+        perpage: int = 1,
+        layer: str = "trafficsigns",
+        value=None
     ):
         bbox_list = [*lowerbbox[::-1], *upperbbox[::-1]]
         bbox = ",".join([repr(point) for point in bbox_list])
         if value is not None:
             params = (
-                f"?layers=trafficsigns&bbox={bbox}&value={value}&per_page={perpage}&"
+                f"?layers={layer}&bbox={bbox}&value={value}&per_page={perpage}&"
                 f"client_id={self.CLIENT_ID}&sort_by=key"
             )
         else:
             params = (
-                f"?layers=trafficsigns&bbox={bbox}&per_page={perpage}&"
+                f"?layers={layer}&bbox={bbox}&per_page={perpage}&"
                 f"client_id={self.CLIENT_ID}&sort_by=key"
             )
         complete_url = self.BASE_DOMAIN + params
