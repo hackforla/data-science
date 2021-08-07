07593c2a54fb3f6220f284d9404bb99a78448b3d
AlbertUlysses <albertulysseschavez@gmail.com>
2021-08-06 18:01:00 -0700

    refactored transformation scripts

--- LAANE/tests/test_assessor_table.py
+++ LAANE/tests/test_assessor_table.py
@@ -5,8 +5,6 @@ import pandas as pd
 from pandas.testing import assert_series_equal
 
 from transformations.assessor_table import (
-    address1,
-    city,
     owner_names,
     special_name_assessee,
     trust_name,
@@ -64,61 +62,3 @@ def test_special_name_assessee():
     ]
     df = pd.DataFrame({"Special Name Assessee": special_names})
     assert_series_equal(special_name_assessee(df), ser)
-
-
-def test_address1():
-    # row 2894
-    items = [
-            "8311 N PEYTON WAY",
-            np.nan,
-            "23135 SCHOENBORN STREET",
-            "23916 1/2 LOS ROSAS STREET",
-            "23931 JENSEN DRIVE"
-    ]
-    ser = pd.Series(items, name="Address1")
-    house_no = [
-            8311,
-            0,
-            23135,
-            23916,
-            23931
-    ]
-    fraction = [
-            "",
-            np.nan,
-            "$  ",
-            "2021-01-02 00:00:00",
-            ""
-    ]
-    direction = [
-            "N",
-            "",
-            "",
-            "",
-            ""
-    ]
-    street_name = [
-            "PEYTON WAY",
-            np.nan,
-            "SCHOENBORN ST",
-            "LOS ROSAS ST",
-            "JENSEN DR    "
-
-    ]
-    df = pd.DataFrame({
-        "Situs House No": house_no,
-        "Fraction": fraction,
-        "Direction": direction,
-        "Street Name": street_name
-        })
-
-    assert_series_equal(address1(df, "Situs House No", "Fraction", "Direction", "Street Name"), ser)
-
-
-def test_city():
-    items = ["LOS ANGELES", "CANOGA PARK", np.nan, 'PALM BEACH GARDENS']
-    ser = pd.Series(items, name="City")
-    city_state = ["LOS ANGELES CA          ", "CANOGA PARK             ", "                        ", 'PALM BEACH GARDENS FL   ']
-    df = pd.DataFrame({"City State": city_state})
-
-    assert_series_equal(city(df["City State"]), ser)
--- LAANE/tests/test_normalize_address.py
+++ LAANE/tests/test_normalize_address.py
@@ -1,10 +1,5 @@
-#!/usr/bin/env python3
 """Tests for exempt_table."""
-import numpy as np
-import pandas as pd
-from pandas.testing import assert_frame_equal
-
-from transformations.tot_table import normalize_address_wrapper
+from transformations.normalize_address import normalize_address_wrapper
 
 
 def test_normalize_address_wrapper():
@@ -50,31 +45,12 @@ def test_normalize_address_wrapper():
 
     assert normalize_address_wrapper(address_with_error) == expected_dict
 
-
-#def test_address():
-#    """Test address function."""
-#    test_list = [
-#        '5251 HOLLYWOOD BLVD',
-#        '1 LMU DRIVE SUITE 2200',
-#        '8405 PERSHING DRIVE UNIT 500',
-#        '7310 S FIGUEROA STREET',
-#        '405 East IMPERIAL HIGHWAY',
-#    ]
-#    test_series = pd.Series(test_list, name='STREET_ADDRESS')
-#    expected_address1 = [
-#        '5251 HOLLYWOOD BLVD',
-#        '1 LMU DR',
-#        '8405 PERSHING DR',
-#        '405 E IMPERIAL HWY',
-#    ]
-#    expected_address2 = [
-#        np.nan,
-#        'STE 2200',
-#        'UNIT 500',
-#        np.nan,
-#    ]
-#    expected_frame = pd.DataFrame({
-#        'Address1': expected_address1,
-#        'Address2': expected_address2,
-#    })
-#    assert_frame_equal(address(test_series), expected_frame)
+    address_with_exempt = '08405 PERSHING DRIVE UNIT 500'
+    expected_dict = {
+        'address_line_1': '8405 PERSHING DR',
+        'address_line_2': 'UNIT 500',
+        'city': None,
+        'state': None,
+        'postal_code': None,
+    }
+    assert normalize_address_wrapper(address_with_address2) == expected_dict
