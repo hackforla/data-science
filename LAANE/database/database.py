0b8a22fb5cefbcf0e6477eb730dbe098e7e87965
AlbertUlysses <albertulysseschavez@gmail.com>
2021-09-21 09:45:37 -0700

    docs;

--- LAANE/database/database.py
+++ LAANE/database/database.py
@@ -6,8 +6,8 @@ Author : Albert Ulysses <albertulysseschavez@gmail.com>
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# unhash below when testing
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./LAANEtest.db'
+# unhash for database below when testing
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./LAANEtest.db'
 # SQLALCHEMY_DATABASE_URL = 'sqlite:///./LAANE.db'
 
 engine = create_engine(
