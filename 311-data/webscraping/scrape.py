e3bab8edb8294b2d93c769ba8eeb5dbaaea9a543
Willa Mannering <57014752+willa-mannering@users.noreply.github.com>
2022-05-17 17:37:40 -0400

    Updated scrape.py
    Script now also outputs table with tech categories, tech urls, and total_count columns

--- 311-data/webscraping/scrape.py
+++ 311-data/webscraping/scrape.py
@@ -1,4 +1,3 @@
-
 from selenium import webdriver
 import validators 
 import time
@@ -38,19 +37,26 @@ def scrape_builtwith(base_url, url_list):
 
     return survey_tech_list
 
-def json_list_to_table(json_list):
-    df_records = pd.DataFrame(json_list)
-    df_records["count"] = 1
-
-    table = pd.pivot_table(df_records, values = "count", index = "tech", columns = "url",aggfunc=np.sum)
+def to_table(data):
+    d = pd.DataFrame(data)
+    d['category'] = d.apply(lambda row: row.href[29:].split('/')[0], axis=1) #get category name from href
+    d['count'] = 1
+   
+    table = pd.pivot_table(d, values = "count", index = "tech", columns = "url",aggfunc=np.sum)
     table.fillna(0,inplace = True)
+   
+    out = pd.merge(d[['tech','href','category']], table, on='tech')
+    output = out.drop_duplicates()
+    col_names = list(output.columns)
 
-    return table
-
+    output.insert(loc=3, column='total_count', value=output[col_names[3:]].sum(axis=1))
+    output = output.rename(columns={'href': 'URL of tech', 'tech' : 'Name of tech'})
+    
+    return output
 
 if __name__ == "__main__":
 
-    print("run")
+    print("Beginning web scraping...")
 
     NCsurvey_df = pd.read_csv('NCsurvey.csv')
     url_list = list(filter(lambda x: x==x, NCsurvey_df.iloc[2,2:].values))  
@@ -62,6 +68,6 @@ if __name__ == "__main__":
     with open('data.json', 'w') as f:
         json.dump(survey_tech_list, f)
 
-    table = json_list_to_table(survey_tech_list)
+    table = to_table(survey_tech_list)
 
-    table.to_csv("tech_table.csv")
+    table.to_csv("tech_table.csv", index=False)
